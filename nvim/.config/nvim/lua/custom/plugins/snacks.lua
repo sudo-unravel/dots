a12c6a7ae4e052b29e9f4c577e05e340bcd4be50
Bálint <petobalint09@gmail.com>
2025-07-17 19:44:04 +0200

    update

--- nvim/.config/nvim/lua/custom/plugins/snacks.lua
+++ nvim/.config/nvim/lua/custom/plugins/snacks.lua
@@ -3,6 +3,14 @@ return {
   priority = 1000,
   lazy = false,
   opts = {
-    dashboard = {},
+    dashboard = {
+      sections = {
+        { section = "header" },
+        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { section = "startup" },
+      },
+},
   },
 }
